1fb1c907c752bd738770388661ca181df7ab9509
DevJoe <joethehumandev@gmail.com>
2018-03-30 23:50:08 -0500

    But wait... There's more!

---
+++ ConsoleApp3/forests/RedWoodForest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleGame.lib;
+
+namespace ConsoleGame.forests
+{
+    class RedWoodForest : Forest
+    {
+        public RedWoodForest() : base("Redwood")
+        {
+            ForestPath JQFalls = new ForestPath("JQ Falls");
+            JQFalls.takePath += JQFalls_takePath;
+            paths = new ForestPath[] { JQFalls };
+        }
+
+        private bool JQFalls_takePath()
+        {
+            Console.Clear();
+            Console.WriteLine("A math master stops you while entering the path.");
+            Console.WriteLine("In order to pass, you must answer a simple math equation.");
+            Console.WriteLine();
+            Console.WriteLine("The equation (24x^2+25x-47)/(ax-2) = (-8x-3)-(53/ax-2) is true for all variables of x ≠ (2/a) where a is a constant\n\nWhat is the value of a?\n\n");
+            Console.Write("Answer: ");
+            int ans = 0;
+            if(!int.TryParse(Console.ReadLine(), out ans))
+            {
+                Console.Clear();
+                Console.WriteLine("The math master has turned you away for your inability to use numbers.");
+                return false;
+            }
+            Console.Clear();
+            if (ans != -3)
+            {
+                Console.Clear();
+                Console.WriteLine("The math master has turned you away for not answering the question correctly.");
+                return false;
+            }
+            Console.WriteLine("You proceed on. Press any key to continue.");
+            Console.ReadKey();
+            return true;
+        }
+    }
+}
---
+++ ConsoleApp3/lib/Forest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleGame.forests;
+
+namespace ConsoleGame.lib
+{
+    class Forest
+    {
+
+        private static Type[] forestTypes = new Type[] { typeof(RedWoodForest) };
+        private static Random random = new Random();
+
+        protected Forest(string name) {
+            this.name = name;
+            this.paths = new ForestPath[] { };
+        }
+
+        public static void displayAndExecutePaths(ForestPath[] paths)
+        {
+            Console.WriteLine("You left are in front of a sign with a bunch of different paths.");
+            Console.WriteLine("0) Leave the forest");
+            for (int i = 0; i < paths.Length; i++)
+            {
+                var p = paths[i];
+                var dispNum = i + 1;
+                Console.WriteLine(dispNum + ") " + p.name);
+            }
+            Console.WriteLine();
+            Console.Write("Which path will you take? ");
+            var path = Console.ReadLine();
+            int pathNum = 0;
+            var parsed = int.TryParse(path, out pathNum);
+            if(!parsed || pathNum < 0 || pathNum > paths.Length)
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid option. Try again.");
+                Console.WriteLine();
+                displayAndExecutePaths(paths);
+                return;
+            }
+            switch(pathNum)
+            {
+                case 0:
+                    Console.WriteLine("You have left the forest.");
+                    return;
+                default:
+                    paths[pathNum-1].take();
+                    break;
+            }
+        }
+
+        public static Forest findForest()
+        {
+            if (forestTypes.Length == 0) return null;
+            var forest = random.Next(0, forestTypes.Length);
+            return (Forest)forestTypes[forest].GetConstructors()[0].Invoke(new object[] { });
+        }
+
+        public void execute()
+        {
+            Console.WriteLine("You walk into the " + this.name + " forest.");
+            displayAndExecutePaths(this.paths);
+        }
+
+        public string name { get; set; }
+        public ForestPath[] paths { get; set; }
+
+        public class ForestPath
+        {
+            public ForestPath(string name) { this.name = name; }
+            public string name { get; set; }
+            public ForestPath[] subpaths = new ForestPath[] { };
+            public delegate bool TakePath();
+            public event TakePath takePath;
+
+            public void take()
+            {
+                bool res = takePath.Invoke();
+                if (!res)
+                {
+                    Console.WriteLine("You have left the forest.");
+                    return;
+                }
+                if(this.subpaths.Length > 0)
+                {
+                    Console.Clear();
+                    Forest.displayAndExecutePaths(subpaths);
+                }
+                Console.Clear();
+                Console.WriteLine("You have reached the end of the forest and decided to leave.");
+            }
+        }
+    }
+}
